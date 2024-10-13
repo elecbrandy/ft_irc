c1e8738b82d0953ddae93fdcee3c77b6462401df
Bessie Ran Choi <bessierchoi@Bessieui-MacBook-Pro.local>
2024-10-14 01:03:06 +0900

    Client setter / getter

--- Client.cpp
+++ Client.cpp
@@ -6,10 +6,32 @@ Client::Client(const std::string& str) : name(str) {}
 
 Client:: ~Client() {}
 
-std::string	Client::getName() const {
-	return this->name;
-}
+/* setter */
+void Client::setNickname(const std::string& str) {this->_nickname = str;}
 
-void Client::setName(const std::string& str) {
-	this->name = str;
-}
+void Client::setUsername(const std::string& str) {this->_username = str;}
+
+void Client::setHostname(const std::string& str) {this->_hostname = str;}
+
+void Client::setRealname(const std::string& str) {this->_realname = str;}
+
+void Client::setPassword(const std::string& str) {this->_password = str;}
+
+void Client::setIpAddress(const std::string& str) {this->_ipAddress = str;}
+
+void Client::setServername(const std::string& str) {this->_servername = str;}
+
+/* getter */
+std::string	Client::getNickname() const {return this->_nickname;}
+
+std::string	Client::getUsername() const {return this->_username;}
+
+std::string	Client::getHostname() const {return this->_hostname;}
+
+std::string	Client::getRealname() const {return this->_realname;}
+
+std::string	Client::getPassword() const {return this->_password;}
+
+std::string	Client::getIpAddress() const {return this->_ipAddress;}
+
+std::string	Client::getServername() const {return this->_servername;}
--- Client.hpp
+++ Client.hpp
@@ -5,15 +5,34 @@
 
 class Client {
 private:
-	std::string	name;
+	std::string	_nickname;
+	std::string	_username;
+	std::string	_hostname;
+	std::string	_realname;
+	std::string _password;
+	std::string _ipAddress; //굳이 필요한가?
+	std::string _servername;
 
 public:
 	Client();
 	Client(const std::string& str);
 	~Client();
 
-	std::string	getName() const;
-	void		setName(const std::string& str);
+	void		setNickname(const std::string& str);
+	void		setUsername(const std::string& str);
+	void		setHostname(const std::string& str);
+	void		setRealname(const std::string& str);
+	void		setPassword(const std::string& str);
+	void		setIpAddress(const std::string& str);
+	void		setServername(const std::string& str); //단일 서버라도 서버이름은 필요함
+
+	std::string	getNickname() const;
+	std::string	getUsername() const;
+	std::string	getHostname() const;
+	std::string	getRealname() const;
+	std::string	getPassword() const;
+	std::string	getIpAddress() const;
+	std::string	getServername() const; 
 };
 
 #endif
